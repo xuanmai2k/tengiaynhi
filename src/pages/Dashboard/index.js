77f193463df95ed48e99453e1527f75a5b05b318
Xuan Mai <xmai0906@gmail.com>
2023-04-08 14:23:27 +0700

    update 8/4

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 import Layout from "../../layouts/Default";
 import Exam06 from "../../components/Exam06";
 const Dashboard =() => {
+    console.log('change')
     return(
         <Layout>
             <Exam06/>
